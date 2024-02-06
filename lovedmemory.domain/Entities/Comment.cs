69972eb9337dcfe9ee3907856aebf6d736a2aecc
Simon Ndiritu <deshtey@gmail.com>
2024-02-06 13:59:56 +0300

    add models

---
+++ lovedmemory.domain/Entities/Comment.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.Design;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace lovedmemory.domain.Entities
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int TributeId { get; set; }
+        public string Details { get; set; }
+        public bool Visible { get; set; } = true;
+        public DateTime DatePosted { get; set; }
+        public List<Comment> Replies { get; set; }
+        public Comment()
+        {
+            Replies = new List<Comment>();
+        }
+
+        public void AddReply(Comment reply)
+        {
+            Replies.Add(reply);
+        }
+        public void Display(int level = 0)
+        {
+            string indentation = new string(' ', level * 2);
+            //Console.WriteLine($"{indentation}Comment ID: {CommentId}");
+            //Console.WriteLine($"{indentation}Text: {Text}");
+            //Console.WriteLine($"{indentation}Date Posted: {DatePosted}");
+
+            foreach (var reply in Replies)
+            {
+                reply.Display(level + 1);
+            }
+        }
+        public virtual Tribute Tribute { get; set; }
+    }
+}
---
+++ lovedmemory.domain/Entities/Tribute.cs
@@ -0,0 +1,16 @@
+﻿namespace lovedmemory.domain.Entities
+{
+    public class Tribute
+    {
+        public int Id { get; set; }
+        public int ViewCount { get; set; }
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public int Owner { get; set; }
+        public string MainImageUrl { get; set; }
+        public DateTime Edited { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime RunDate { get; set; }
+        public bool Active { get; set; }
+    }
+}
